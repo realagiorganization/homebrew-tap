0303051536e1f27f6ed276516b901068f10a9e50
standard@kali-thinkpad1 <standard@kali-thinkpad1.realagiorganization>
2026-04-02 13:09:48 +0700

    Sync release formulas for tether:v0.3.7-rc2

--- Formula/tether-ai.rb
+++ Formula/tether-ai.rb
@@ -2,14 +2,14 @@ class TetherAi < Formula
   include Language::Python::Virtualenv
   desc "Tether self-contained package"
   homepage "https://gettether.dev"
-  url "https://github.com/Adminrealagi/tether/releases/download/v0.3.7-rc1/tether-ai-v0.3.7-rc1-wheelhouse.tar.gz"
-  sha256 "ecf82818a9720dbbacf48790c43fe2ad8a5bc642eae128569625eb377a8aed78"
-  version "0.3.7-rc1"
+  url "https://github.com/Adminrealagi/tether/releases/download/v0.3.7-rc2/tether-ai-v0.3.7-rc2-wheelhouse.tar.gz"
+  sha256 "7ae8d07b1d77970fc7ae77ce32f4c80113e73a8522f8543150ee933dfc2ca620"
+  version "0.3.7-rc2"
   depends_on "python@3.12"
 
   def install
     venv = virtualenv_create(libexec, "python3.12")
-    system libexec/"bin/pip", "install", "--find-links=#{buildpath}", "tether-ai[discord] @ file://#{buildpath}/tether_ai-0.3.7rc1-py3-none-any.whl"
+    system libexec/"bin/pip", "install", "--find-links=#{buildpath}", "tether-ai[discord] @ file://#{buildpath}/tether_ai-0.3.7rc2-py3-none-any.whl"
     bin.install_symlink libexec/"bin/tether" => "tether"
     bin.install_symlink libexec/"bin/tether-mcp" => "tether-mcp"
   end
